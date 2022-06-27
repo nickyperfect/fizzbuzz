5208509b807b3826e98e8824254d0fe05df3aec1
Nick Rogers <nick@aliensfromthefuture.com>
2022-06-27 15:26:34 -0500

    removed repetitive references

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    for fizzy in 1..100{
+    for fizzy in 1..101{
         match fizzy {
-            fizzy if (fizzy %15 == 0) => println!("{:?}", "FIZZ-BUZZ"), 
-            fizzy if (fizzy %3 == 0) => println!("{:?}", "FIZZ"), 
-            fizzy if (fizzy %5 == 0) => println!("{:?}", "BUZZ"), 
-            _ => println!("{:?}", fizzy)
+            _ if fizzy % 15 == 0 => println!("FIZZ-BUZZ"), 
+            _ if fizzy % 3 == 0 => println!("FIZZ"), 
+            _ if fizzy % 5 == 0 => println!("BUZZ"), 
+            _ => println!("{}", fizzy),
         }
     }
 }
